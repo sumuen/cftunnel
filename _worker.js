26c920307cf062a1fbd2d1a2bba74b1df4a50fdf
sumuen <zim29144@gmail.com>
2023-10-05 12:03:44 +0800

    1

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '7eac2715-91bd-45a8-ed5b-e8fff64dbe73';
+let userID = '73d9b96e-4c84-4174-ae3d-224828f49eba';
 
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 
